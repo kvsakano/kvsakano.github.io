fbbc85e3fa26fd6ee3509f45c3b9d553af7896f8
Kristy Sakano <kvsakano@gmail.com>
2025-10-24 19:07:12 -0400

    updated the cv

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "To download my CV as a PDF, click the button to the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
